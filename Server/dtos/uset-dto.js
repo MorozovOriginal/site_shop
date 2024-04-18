70728f0ba09aaf6195a13c13aec87b1885f7b81b
MorozovOriginal <123456789dimabos@gmail.com>
2024-04-18 23:41:33 +0300

    V1
    V1

---
+++ Server/dtos/uset-dto.js
@@ -0,0 +1,12 @@
+module.exports = class UserDto {
+    email;
+    name;
+    id;
+    isActivated;
+    constructor(model) {
+        this.email = model.email;
+        this.name = model.name;
+        this.id = model.id;
+        this.isActivated = model.isActivated;
+    }
+}
